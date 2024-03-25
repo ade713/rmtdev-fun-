14634478a9b3efb3ec4513c40cceedd0dc3774c3
Ade Farquhar <adefarquhar@Ades-MacBook-Air.local>
2024-03-25 16:41:23 -0700

    - place popover in a portal w/ `usePortal` on doc body to avoid potential z-index issues

--- src/components/BookmarksPopover.tsx
+++ src/components/BookmarksPopover.tsx
@@ -1,13 +1,15 @@
 import { forwardRef } from "react";
+import { createPortal } from "react-dom";
 import { useBookmarksContext } from "../lib/hooks";
 import JobList from "./JobList";
 
 export const BookmarksPopover = forwardRef<HTMLDivElement>(function (_, ref) {
   const { bookmarkedJobItems, isLoading } = useBookmarksContext();
 
-  return (
+  return createPortal(
     <div className="bookmarks-popover" ref={ref}>
       <JobList jobItems={bookmarkedJobItems} isLoading={isLoading} />
-    </div>
+    </div>,
+    document.body
   );
 });
