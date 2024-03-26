915016ea45219712915bdbe060e336ffa0cd36bb
Ade Farquhar <adefarquhar@Ades-MacBook-Air.local>
2024-03-26 16:23:55 -0700

    - use useMemo on sorted and slice job items arrays to reduce unnecessary operations

--- src/contexts/JobItemsContextProvider.tsx
+++ src/contexts/JobItemsContextProvider.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useMemo, useState } from "react";
 import { RESULTS_PER_PAGE } from "../lib/constants";
 import { useSearchQuery, useSearchTextContext } from "../lib/hooks";
 import { SortBy, PageDirection, TJobItem } from "../lib/types";
@@ -33,14 +33,20 @@ export function JobItemsContextProvider({ children }: {
   const totalNumberOfPages = totalNumberOfResults / RESULTS_PER_PAGE;
   const sliceStartIndex = currentPage * RESULTS_PER_PAGE - RESULTS_PER_PAGE;
   const sliceEndIndex = currentPage * RESULTS_PER_PAGE;
-  const jobItemsSorted = [...(jobItems || [])].sort((a, b) => {
-    if (sortBy === 'relevant') {
-      return b.relevanceScore - a.relevanceScore;
-    } else {
-      return a.daysAgo - b.daysAgo;
-    }
-  }) || [];
-  const jobItemsSortedAndSliced = jobItemsSorted?.slice(sliceStartIndex, sliceEndIndex);
+  const jobItemsSorted = useMemo(
+    () => [...(jobItems || [])].sort((a, b) => {
+      if (sortBy === 'relevant') {
+        return b.relevanceScore - a.relevanceScore;
+      } else {
+        return a.daysAgo - b.daysAgo;
+      }
+    }),
+    [sortBy, jobItems]
+  );
+  const jobItemsSortedAndSliced = useMemo(() =>
+    jobItemsSorted?.slice(sliceStartIndex, sliceEndIndex),
+    [sliceStartIndex, sliceEndIndex, jobItemsSorted]
+  );
 
   // event handlers / actions
   const handleChangePage = (direction: PageDirection) => {
